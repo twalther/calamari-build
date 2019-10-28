91890056e196ec5e98192aa477ab2bcb8f407797
Torbjörn Walther <torbjorn.walther@gmail.com>
2019-10-28 13:39:58 +0100

    Save .giversion.txt file

--- build.sh
+++ build.sh
@@ -29,15 +29,13 @@ cd artifacts
 
 export GIT_VERSION=$(cat .gitversion.txt)
 
-rm .gitversion.txt
-
 tar -cvzf calamari-rhel.6-x64-$GIT_VERSION.tar.gz *
 
 if ! [ -d "../artifacts-history" ]; then
 	mkdir ../artifacts-history
 fi
 
-cp calamari-rhel.6-x64-$GIT_VERSION.tar.gz ../artifacts-history
+cp calamari-rhel.6-x64-$GIT_VERSION.tar.gz --exclude='.gitversion.txt' ../artifacts-history
 
 echo "Created artifacts/calamari-rhel.6-x64-$GIT_VERSION.tar.gz"
 echo "Copied to artifacts-history/calamari-rhel.6-x64-$GIT_VERSION.tar.gz"
