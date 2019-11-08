5a425cf182de97fef26104beac0f3b4948c1eea1
Torbjörn Walther <torbjorn.walther@gmail.com>
2019-11-08 22:00:54 +0100

    Add version sort

--- files/gitversion.sh
+++ files/gitversion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GIT_VERSION=$(git tag | egrep "^[0-9]{1,2}\.[0-9]+\.[0-9]+$" | sort -r | head -1)
+GIT_VERSION=$(git tag | egrep "^[0-9]{1,2}\.[0-9]+\.[0-9]+$" | sort -V -r | head -1)
 
 echo "$GIT_VERSION" > .gitversion.txt
 
