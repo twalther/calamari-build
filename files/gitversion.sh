6d177849e17c9e19f269ff7c9ae50482a171b04e
Torbjörn Walther <torbjorn.walther@gmail.com>
2019-11-11 10:12:38 +0100

    Use GIT_VERSION if set

--- files/gitversion.sh
+++ files/gitversion.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-GIT_VERSION=$(git tag | egrep "^[0-9]{1,2}\.[0-9]+\.[0-9]+$" | sort -V -r | head -1)
+if ! [ -n "$GIT_VERSION" ]; then
+	GIT_VERSION=$(git tag | egrep "^[0-9]{1,2}\.[0-9]+\.[0-9]+$" | sort -V -r | head -1)
+fi
 
 echo "$GIT_VERSION" > .gitversion.txt
 
